816c2ca434d2013b1872b4392eda873264b9c183
PabloPerezAlbela <p.perezalbela@pucp.edu.pe>
2022-11-27 19:08:31 -0500

    Logica de colapso implementada, falta comprobar pq aun no me se la logica completa de pablo pero intente hacer lo que me dijo xdxd

--- src/main/java/pucp/dp1/redex/controller/route/FlightPlanController.java
+++ src/main/java/pucp/dp1/redex/controller/route/FlightPlanController.java
@@ -1,15 +1,16 @@
 package pucp.dp1.redex.controller.route;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import pucp.dp1.redex.model.response.Estado;
 import pucp.dp1.redex.model.response.ResponseObject;
@@ -30,6 +31,26 @@ public class FlightPlanController {
 		try {
 			List<FlightPlan> lista = this.service.findAll();
 			response.setResultado(lista);
+			response.setEstado(Estado.OK);
+			return new ResponseEntity<ResponseObject>(response, HttpStatus.OK);
+		} catch(Exception e) {
+			response.setError(1, "Error", e.getMessage());
+			response.setEstado(Estado.ERROR);
+			return new ResponseEntity<ResponseObject>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	@GetMapping(path = "/allDay", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ResponseObject> consultarTodosPorDia(@RequestParam("fecha") String fecha) {
+		ResponseObject response = new ResponseObject();
+		try {
+			String datereq = fecha.substring(1, 11).replace("-", "");
+			Date dateDate;
+			SimpleDateFormat formatterDate = new SimpleDateFormat("yyyyMMdd");
+			dateDate = formatterDate.parse(datereq);
+			LocalDate date1 = LocalDate.parse(new SimpleDateFormat("yyyy-MM-dd").format(dateDate));
+			List<FlightPlan> lista = this.service.findAll();
+			List<FlightPlan> listaFiltrada = lista.stream().filter(x -> x.getTakeOffDate().equals(date1)).collect(Collectors.toList());
+			response.setResultado(listaFiltrada);
 			response.setEstado(Estado.OK);
 			return new ResponseEntity<ResponseObject>(response, HttpStatus.OK);
 		} catch(Exception e) {
