4d2b268e1945be463f76e952cdc255df4e060b97
Veneko <john.soto@pucp.edu.pe>
2022-12-03 00:44:27 -0500

    Imports terminados ahora si

--- src/main/java/pucp/dp1/redex/dao/sales/IHistorico.java
+++ src/main/java/pucp/dp1/redex/dao/sales/IHistorico.java
@@ -1,30 +1,19 @@
 package pucp.dp1.redex.dao.sales;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import pucp.dp1.redex.model.route.FlightPlan;
 import pucp.dp1.redex.model.sales.Historico;
-
 public interface IHistorico extends JpaRepository<Historico, Integer> {
-
     List<Historico> findAll();
-
     Optional<Historico> findById(Integer id);
-
-    //pattern "dd/MM/yyyy"
     List<Historico> findByFecha(LocalDate fecha);
-
-
     @Query("SELECT h FROM Historico h WHERE h.fecha = :fecha AND h.hora BETWEEN :horaI AND  :horaF order by h.hora")
     List<Historico> findByFechaHora(LocalDate fecha, LocalTime horaI, LocalTime horaF);
 }
-
